5eb5846037de85a3a97b2aa2d29b5232ddbc0891
Chocomint <86723911+ChocomintSSR@users.noreply.github.com>
2021-11-22 16:50:30 +0800

    Create unit.h

---
+++ Category-Algorithm/unit/unit.h
@@ -0,0 +1,17 @@
+#ifndef _UNIT_H_
+#define _UNIT_H_
+
+#include <iostream>
+#include <vector>
+#include <pair>
+
+namespace chocomint
+{
+    class unit
+    {
+    private:
+        vector<pair<string, string>> v;
+    };
+}
+
+#endif
