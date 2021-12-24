527b536f0a7d77bcfbccf43c5944531c268de000
Chocomint <86723911+ChocomintSSR@users.noreply.github.com>
2021-12-24 23:37:12 +0800

    upload common.h

--- Category-Math/complexPre/literal_test.cpp
+++ Category-Math/complexPre/literal_test.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
+#define ull unsigned long long int
 
-long double operator"" kg(long double x)
+ull operator"" kg(ull x)
 {
 	return x * 1000;
 }
 
 int main()
 {
-	long double kg = 12kg;
+	auto k = 12kg;
 	return 0;
 }
---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <iostream>
+
+namespace std
+{
+    
+}
+
+#endif // _COMMON_H_
